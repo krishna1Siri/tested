abe1cd4cb63aa1ab5bae4a376e14bdfed0400de8
krishna1Siri <Krishna.forntend17@com>
2021-04-25 18:58:22 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { ModeldrivenComponent } from './modeldriven/modeldriven.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ModeldrivenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
